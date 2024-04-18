20fb9f69dee51e4e1a286073bfe9c897d908a09d
Jay Rogers <jay@521dimensions.com>
2024-04-18 16:42:12 -0500

    Update change-volume-permissions.sh script with new options and help menu

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # docker-volume-change-permission-script
-A helpful script for assisting in the migration of serversideup/php v2 → v3
+A helpful script for assisting in the migration of serversideup/php v2 → v3.
+
+> [!CAUTION]
+> Be sure to have tested backups before running this script. Root permissions are required, and the script will change the ownership of directories within the Docker volumes base directory. Use this script at your own risk.
 
 ### Usage
 ```bash
-bash change-volume-permissions.sh <prefix> <user:group> [--dry-run]
+bash change-volume-permissions.sh --volume-prefix <prefix> --permissions <user:group> [--dry-run]
 ```
+
+###### Options
+- `--volume-prefix`: Specific prefix of directories to search within the Docker volumes base directory."
+- `--permissions`: User and group to change ownership to, formatted as user:group."
+- `--dry-run`: Optional flag to simulate changes without applying them."
 
 ### More help
 Read our migration guide: https://serversideup.net/open-source/docker-php/docs/guide/migrating-from-v2-to-v3
--- change-volume-permissions.sh
+++ change-volume-permissions.sh
@@ -1,15 +1,32 @@
+
 #!/bin/bash
 
 # Script Name: change-volume-permissions.sh
-# Usage: bash change-volume-permissions.sh <prefix> <user:group> [--dry-run]
+# Usage: bash change-volume-permissions.sh --volume-prefix <prefix> --permissions <user:group> [--dry-run]
+
+DOCKER_VOLUME_BASE_DIR=${DOCKER_VOLUME_BASE_DIR:-"/var/lib/docker/volumes"}
+
+# Initialize variables
+VOLUME_PREFIX=""
+USER_GROUP=""
+DRY_RUN=false
 
 # Function to show help menu
 show_help() {
-    echo "Usage: $0 <prefix> <user:group> [--dry-run]"
-    echo "prefix       Prefix of directories to search within the Docker volumes base directory."
-    echo "user:group   User and group to change ownership to, formatted as user:group."
-    echo "--dry-run    Optional flag to simulate changes without applying them."
+    echo
+    echo "ℹ️ Usage: $0 --volume-prefix <prefix> --permissions <user:group> [--dry-run]"
+    echo "--volume-prefix   Specific prefix of directories to search within the Docker volumes base directory."
+    echo "--permissions     User and group to change ownership to, formatted as user:group."
+    echo "--dry-run         Optional flag to simulate changes without applying them."
+    echo
+    echo "👇 Available volume prefixes:"
+    list_unique_volume_prefixes
     exit 1
+}
+
+list_unique_volume_prefixes() {
+    find "${DOCKER_VOLUME_BASE_DIR}" -mindepth 1 -maxdepth 1 -type d \
+        -exec basename {} \; | awk -F'_' '{print $1}' | sort | uniq
 }
 
 # Check if the script is running as root
@@ -18,16 +35,23 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-# Check for the correct number of arguments
-if [[ $# -lt 2 ]] || [[ $# -gt 3 ]]; then
+# Parse command line options
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        --volume-prefix) VOLUME_PREFIX="$2"; shift ;;
+        --permissions) USER_GROUP="$2"; shift ;;
+        --dry-run) DRY_RUN=true ;;
+        -h|--help) show_help ;;
+        *) echo "Unknown option: $1" >&2; exit 1 ;;
+    esac
+    shift
+done
+
+# Check if required options are set
+if [[ -z "$VOLUME_PREFIX" ]] || [[ -z "$USER_GROUP" ]]; then
+    echo "Error: --volume-prefix and --permissions are required."
     show_help
 fi
-
-# Assign variables
-PREFIX=$1
-USER_GROUP=$2
-DRY_RUN=$3
-DOCKER_VOLUME_BASE_DIR="/var/lib/docker/volumes"
 
 # Check for valid user:group format
 if ! [[ $USER_GROUP =~ ^[0-9]+:[0-9]+$ ]]; then
@@ -36,16 +60,16 @@ if ! [[ $USER_GROUP =~ ^[0-9]+:[0-9]+$ ]]; then
 fi
 
 # Display overview of directories to be updated
-echo "The following directories will be affected:"
-for dir in ${DOCKER_VOLUME_BASE_DIR}/${PREFIX}*; do
-    if [[ -d "$dir" && "$(basename "$dir")" = "${PREFIX}"* ]]; then
+echo "The following directories with volume prefix '${VOLUME_PREFIX}' will be affected:"
+for dir in ${DOCKER_VOLUME_BASE_DIR}/${VOLUME_PREFIX}*; do
+    if [[ -d "$dir" && "$(basename "$dir")" = "${VOLUME_PREFIX}"* ]]; then
         echo "${dir}/_data/"
     fi
 done
 
 # Prompt for confirmation if not a dry run
-if [[ "$DRY_RUN" != "--dry-run" ]]; then
-    read -p "Do you want to proceed with changing ownership? (y/N) " -n 1 -r
+if [[ "$DRY_RUN" != "true" ]]; then
+    read -p "Are you sure you want to change the ownership of these directories? (y/N) " -n 1 -r
     echo    # Move to a new line
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then
         [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # Handle execution source accordingly.
@@ -60,7 +84,7 @@ change_ownership() {
     local target_path="${DOCKER_VOLUME_BASE_DIR}/${folder}/_data"
 
     if [[ -d "$target_path" ]]; then
-        if [[ "$dry_run" == "--dry-run" ]]; then
+        if [[ "$dry_run" == "true" ]]; then
             echo "DRY RUN: Would change ownership of $target_path to $user_group"
         else
             chown -R $user_group "$target_path"
@@ -71,10 +95,15 @@ change_ownership() {
 }
 
 # Main loop to change ownership if confirmed
-if [[ "$DRY_RUN" == "--dry-run" ]] || [[ $REPLY =~ ^[Yy]$ ]]; then
-    for dir in ${DOCKER_VOLUME_BASE_DIR}/${PREFIX}*; do
-        if [[ -d "$dir" && "$(basename "$dir")" = "${PREFIX}"* ]]; then
+if [[ "$DRY_RUN" == "true" ]] || [[ $REPLY =~ ^[Yy]$ ]]; then
+    for dir in ${DOCKER_VOLUME_BASE_DIR}/${VOLUME_PREFIX}*; do
+        if [[ -d "$dir" && "$(basename "$dir")" = "${VOLUME_PREFIX}"* ]]; then
             change_ownership "$(basename "$dir")" "$USER_GROUP" "$DRY_RUN"
         fi
     done
+    if [[ "$DRY_RUN" == "true" ]]; then
+        echo "DRY RUN: No changes were applied."
+    else
+        echo "✅ Ownership changes applied successfully."
+    fi
 fi
